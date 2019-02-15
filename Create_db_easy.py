6c4464d3b641596f5c855cbeed8ef1a2210eed2b
Kostulski Marcin <marcin.kostulski@immalle.eu>
2019-02-15 15:17:28 +0100

    initialcommit,error included

--- Create_db_easy.py
+++ Create_db_easy.py
@@ -5,21 +5,37 @@ db_filename = 'leerplandoelen_db.sqlite3'
 conn = sqlite3.connect(db_filename)
 c = conn.cursor()
 
+c.execute("CREATE TABLE Competentie(nummer INTEGER, omschrijving TEXT);")
+c.execute("CREATE TABLE Deelconpetenties(nummer INTEGER, omschrijving TEXT);")
+c.execute("CREATE TABLE Leerplandoelen(nummer INTEGER, omschrijving TEXT)")
+
 with open('leerplandoelen_text.txt', 'r') as f:
     for line in f:
         if line.startswith("Competentie"):
+            table = "Competentie"
             dashIndex = line.index('-')
             nummer = line[len('Competentie'):dashIndex]
-            omschrijving = line[dashIndex+1:]
+            omschrijving = str(line[dashIndex+1:])
             print(f"C {nummer} {omschrijving}")
-            #c.execute()
+            c.execute(f"INSERT INTO {table} VALUES({nummer}, {omschrijving})")
+
         elif line.startswith("Deelcompetentie"):
-            print("D")
+            table = "Deelcompetenties"
+            dashIndex = line.index('-')
+            nummer = line[len('Deelcompetentie'):dashIndex]
+            omschrijving = str(line[dashIndex+1:])
+            print(f"D {nummer} {omschrijving}")
+            c.execute(f"INSERT INTO {table} VALUES({nummer}, {omschrijving})")
+
         else:
             if line.strip() == "":
                 print("EMPTY")
             else:
-                print("L")
+                table = "Leerplandoelen"
+                nummer = line[0:5]
+                omschrijving = str(line[len(nummer):])
+                print(f"L {nummer} {omschrijving}")
+                c.execute(f"INSERT INTO {table} VALUES({nummer}, {omschrijving})")
 
-conn.commit()
+    conn.commit()
 c.close
